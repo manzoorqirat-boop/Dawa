6e0f002164a528ca648c20ab2331dd109b8f638a
manzoorqirat-boop <manzoorqirat@gmail.com>
2026-03-31 17:25:21 +0530

    Update user.routes.js

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,1 +1,13 @@
-jj
+const router = require("express").Router();
+const {
+  getUsers, createUser, updateUser, deleteUser, resetPassword,
+} = require("../controllers/user.controller");
+const { protect, authorize } = require("../middleware/auth");
+
+router.use(protect, authorize("admin")); // all user routes: admin only
+
+router.route("/").get(getUsers).post(createUser);
+router.route("/:id").put(updateUser).delete(deleteUser);
+router.put("/:id/reset-password", resetPassword);
+
+module.exports = router;
